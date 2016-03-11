2d0200caf9b0ede87834812bc0344a250988d0af
gillchristian <gillchristiang@gmail.com>
2016-03-11 02:57:00 -0300

    packages and config

--- app/config.js
+++ app/config.js
@@ -2,13 +2,13 @@
 
 let config = {
   db: {
-    host: 'localhost',
-    user: 'root',
-    password: null,
-    database: 'font-awesome-store'
+    host: process.env.host || 'localhost',
+    user: process.env.user || 'root',
+    password: process.env.password || null,
+    database: process.env.db || 'font-awesome-store'
   },
   port: process.env.PORT || 3000,
-  secret: 'NqtIfs/S52F6]||NM&s^dj6IP3$o`brn&Au15%d]vk(W2{<PSFo#bqYaZ`P]43fw'
+  secret: 'nodeRocks'
 }
 
 module.exports = config;
